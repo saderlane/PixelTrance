2ad8921b5c3f73074084b0d3d5a7404b99af4017
saderlane <saderlane5@gmail.com>
2025-03-29 18:32:59 -0400

    starting the fuck over

---
+++ src/client/java/saderlane/pixeltrance/PixelTranceClient.java
@@ -0,0 +1,10 @@
+package saderlane.pixeltrance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixelTranceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
