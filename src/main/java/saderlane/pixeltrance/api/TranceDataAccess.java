8b8bf62f949bd55bf561404aafe27009bbf99a05
saderlane <saderlane5@gmail.com>
2026-05-22 16:54:53 -0700

    Clearing the project to build up in neoforge

--- src/main/java/saderlane/pixeltrance/api/TranceDataAccess.java
+++
@@ -1,7 +0,0 @@
-package saderlane.pixeltrance.api;
-
-import saderlane.pixeltrance.data.TranceData;
-
-public interface TranceDataAccess {
-    TranceData getTranceData();
-}
--- src/main/java/saderlane/pixeltrance/data/PixelTranceUUID.java
+++
@@ -1,9 +0,0 @@
-package saderlane.pixeltrance.data;
-
-import java.util.UUID;
-
-public class PixelTranceUUID {
-
-    public static final UUID TRANCE_SLOW_MODIFIER = UUID.fromString("9b49131e-56e3-4471-9f5e-2cdfd97951ae");
-
-}
