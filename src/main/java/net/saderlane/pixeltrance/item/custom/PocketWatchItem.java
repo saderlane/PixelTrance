c631d5a278421992a12f0be2a530ed625d08520b
saderlane <saderlane5@gmail.com>
2026-05-26 21:09:29 -0700

    Sets pocket watch ticking = false if it is taken out of the player's main or offhand

--- src/main/java/net/saderlane/pixeltrance/item/custom/PocketWatchItem.java
+++ src/main/java/net/saderlane/pixeltrance/item/custom/PocketWatchItem.java
@@ -6,12 +6,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import net.saderlane.pixeltrance.dev.PTLog;
 import net.saderlane.pixeltrance.util.ModTags;
 
@@ -69,6 +71,18 @@ public class PocketWatchItem extends Item {
 
         return super.use(level, player, usedHand);
     }
+
+    // Stops pocket watch ticking if it is not in the player's hand or offhand
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+        if (!level.isClientSide) {
+            if ((!isSelected && (entity instanceof Player && ((Player)entity).getOffhandItem() != stack))) {
+                ticking = false;
+                //PTLog.debug("[PixelTrance] Ticking: " + ticking);
+            }
+        }
+    }
+
 
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
