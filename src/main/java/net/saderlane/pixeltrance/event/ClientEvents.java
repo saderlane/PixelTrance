e42d9b4603fba268889742fb3f1e27c4c05b34d4
saderlane <saderlane5@gmail.com>
2026-08-14 17:04:29 -0700

    added core of trance bar - no visuals yet

--- src/main/java/net/saderlane/pixeltrance/event/ClientEvents.java
+++ src/main/java/net/saderlane/pixeltrance/event/ClientEvents.java
@@ -3,15 +3,19 @@ package net.saderlane.pixeltrance.event;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.saderlane.pixeltrance.PixelTrance;
 import net.saderlane.pixeltrance.client.ClientHypnoCache;
+import net.saderlane.pixeltrance.dataattachment.ModData;
 import net.saderlane.pixeltrance.networking.packet.HypnoDataS2C;
 import net.saderlane.pixeltrance.util.ModKeyMappings;
 
@@ -42,6 +46,40 @@ public class ClientEvents {
             player.sendSystemMessage(Component.literal(
                     "Focus: " + ClientHypnoCache.getFocus() + " / 100"));
         }
+    }
+
+    @SubscribeEvent
+    public static void registerHUD(RegisterGuiLayersEvent event) {
+        event.registerAbove(VanillaGuiLayers.HOTBAR, ResourceLocation.fromNamespaceAndPath(PixelTrance.MOD_ID, "trance_bar"),
+            (guiGraphics, deltaTracker) -> {
+                int x = guiGraphics.guiWidth() / 2; // Midpoint of X
+                int y = guiGraphics.guiHeight();
+
+                // Icon for empty trance icon
+                if(!Minecraft.getInstance().player.isCreative() && Minecraft.getInstance().player.hasData(ModData.TRANCE)) {
+                    for (int i = 0; i < 10; i++) { //Draw 10 empty icons
+                        guiGraphics.blitSprite(ResourceLocation.fromNamespaceAndPath(PixelTrance.MOD_ID, "trance_icon_bg"),
+                                21, 21, 0, 0, x -95 + i * 18, y - 55, 16, 16);
+                    }
+                }
+
+                // Icon for partial trance icon
+                if(!Minecraft.getInstance().player.isCreative() && Minecraft.getInstance().player.hasData(ModData.TRANCE)) {
+                    for (int i = 0; i < 5; i++) {
+                        guiGraphics.blitSprite(ResourceLocation.fromNamespaceAndPath(PixelTrance.MOD_ID, "trance_icon_partial"),
+                                21, 21, 0, 0, x -95 + i * 18, y - 55, 16, 16);
+                    }
+                }
+
+                // Icon for full trance icon
+                if(!Minecraft.getInstance().player.isCreative() && Minecraft.getInstance().player.hasData(ModData.TRANCE)) {
+                    for (int i = 0; i < 5; i++) {
+                        guiGraphics.blitSprite(ResourceLocation.fromNamespaceAndPath(PixelTrance.MOD_ID, "trance_icon_full"),
+                                21, 21, 0, 0, x -95 + i * 18, y - 55, 16, 16);
+                    }
+                }
+
+            });
     }
 
     // When logging out, clear the cache
